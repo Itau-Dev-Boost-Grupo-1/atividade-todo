59e48041a2235c74c58e67a187df443f3bfb1fb7
Faiton <franklin.veiga@processmind.com.br>
2020-09-05 10:03:52 -0300

    inicial do user todos

---
+++ src/components/UserTodos/UserTodos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function UserTodos(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+UserTodos.propTypes = {
+  userId: PropTypes.string
+}
+
+export default UserTodos
+
+
