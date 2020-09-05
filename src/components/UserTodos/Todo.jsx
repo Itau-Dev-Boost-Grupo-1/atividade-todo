f0b40866679c2129fbe7cc7ff8d98167d3273bc9
Faiton <franklin.veiga@processmind.com.br>
2020-09-05 10:20:10 -0300

    todos

---
+++ src/components/UserTodos/Todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Todo(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Todo.propTypes = {
+  data: PropTypes.object.isRequired
+}
+
+export default Todo
+
--- src/components/UserTodos/UserTodos.jsx
+++ src/components/UserTodos/UserTodos.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Todo from './Todo'
+
 function UserTodos(props) {
   return (
     <div>
